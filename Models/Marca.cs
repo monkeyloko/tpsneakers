adf99361c64a36c4ac29d39814d7f41d20f606e5
monkeyloko <defesanti@gmail.com>
2022-10-30 18:38:57 -0300

    views

--- Models/Marca.cs
+++ Models/Marca.cs
@@ -1,66 +1,89 @@
 namespace TPSNEAKERS.Models;
 
 
-public class Marca{
+public class Marca
+{
     private int _ID_MARCA;
     private string _Nombre;
     private string _Fundadores;
     private string _Foto;
     private DateTime _FechaFundacion;
-   
 
-    public Marca(int ID_MARCA, string Nombre, string Fundadores, string Foto, DateTime FechaFundacion){
+
+    public Marca(int ID_MARCA, string Nombre, string Fundadores, string Foto, DateTime FechaFundacion)
+    {
         _ID_MARCA = ID_MARCA;
-        _Nombre =Nombre;
+        _Nombre = Nombre;
         _Fundadores = Fundadores;
         _Foto = Foto;
         _FechaFundacion = FechaFundacion;
-      
+
     }
 
-    public int ID_MARCA{
-        get{
+    public int ID_MARCA
+    {
+        get
+        {
             return _ID_MARCA;
-        } set{
+        }
+        set
+        {
             _ID_MARCA = value;
         }
     }
 
-    public string Nombre{
-        get{
+    public string Nombre
+    {
+        get
+        {
             return _Nombre;
-        } set{
+        }
+        set
+        {
             _Nombre = value;
         }
     }
-   
-    public string Foto{
-        get{
+
+    public string Foto
+    {
+        get
+        {
             return _Foto;
-        } set{
+        }
+        set
+        {
             _Foto = value;
         }
     }
-    public string Fundadores{
-        get{
+    public string Fundadores
+    {
+        get
+        {
             return _Fundadores;
-        } set{
+        }
+        set
+        {
             _Fundadores = value;
         }
     }
-        public DateTime FechaFundacion{
-        get{
+    public DateTime FechaFundacion
+    {
+        get
+        {
             return _FechaFundacion;
-        } set{
+        }
+        set
+        {
             _FechaFundacion = value;
         }
     }
-    public Marca(){
-     
+    public Marca()
+    {
+
     }
 
 
-  
+
 
 
 }
