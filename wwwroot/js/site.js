8a5bcc994d13ffea3a80a2e3855e41e2aa0182dd
monkeyloko <defesanti@gmail.com>
2022-11-05 17:21:15 -0300

    Redisenio views (doff)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// remap jQuery to $
+(function ($) {
+
+    /* trigger when page is ready */
+    $(document).ready(function () {
+
+        // your functions go here
+        $('#email-field').click(function () {
+            $(this).addClass("active");
+            $(this).attr('placeholder', 'Ingrese un nombre');
+            $('#subscribe-button').addClass("show");
+        });
+
+    });
+
+})(window.jQuery);
